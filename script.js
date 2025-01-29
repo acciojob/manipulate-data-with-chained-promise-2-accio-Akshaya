f1669430d384bc1015bd28ac98b6fc94ccb3c326
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-28 21:38:39 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function getNumbers(){
+	return new promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000);
+	});
+}
+
+getNumbers().then((numbers)=>{
+	return new promise((resolve)=>{
+	   setTimeout(()=>{
+		   const evens = numbers.filter(num => num%2 === 0);
+		   document.getElementById("output").textContent =`Filter Evens: ${even.join(", ")}`;
+		   resolve(evens);
+	   },1000);
+	});
+})
+
+.then((evenNumbers) =>{
+	return new promise ((resolve)=>{
+		setTimeout(()=>{
+			const doubled = evenNumbers.map(num => num*2);
+			document.getElementById("output").textContent = `Doubled Evens: ${doubled.join(", ")}`;
+			resolve(doubled);
+		},2000);
+	});
+});
